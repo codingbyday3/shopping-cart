09e0f780a9e6a7dfa11df28e1672938787b9b212
Klemen <codingbyday3@users.noreply.github.com>
2025-08-03 17:27:07 +0200

    Add: content to homepage

--- src/components/HomePage/HomePage.jsx
+++ src/components/HomePage/HomePage.jsx
@@ -1,8 +1,17 @@
+import { Link } from "react-router-dom"
+
 const HomePage = () =>{
     return(
-        <div>
-            <h1>Hello to homepage</h1>
-        </div>
+        <section>
+            <div className="slide-show">
+
+            </div>
+            <div>
+                <h2>About us</h2>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Vero quae ipsa dolor eaque aperiam explicabo error ullam aliquid aut itaque, doloribus praesentium dolores laborum non odit sunt deleniti. Ea, fugiat!</p>
+            </div>
+            <Link to="/shop">Start shopping</Link>
+        </section>
     )
 }
 
