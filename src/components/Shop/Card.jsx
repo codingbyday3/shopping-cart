ca40e3d40aa4af145a6ddc20887847e68cd6241e
Klemen <codingbyday3@users.noreply.github.com>
2025-08-04 09:15:33 +0200

    Style: shop

--- src/components/Shop/Card.jsx
+++ src/components/Shop/Card.jsx
@@ -1,14 +1,16 @@
+import styles from "./Shop.module.css"
+
 const Card = () =>{
     return(
-        <div className="card">
+        <div className={styles.card}>
             <img src="" alt="" />
             <strong>Shirt</strong>
             <p>Price: 10$</p>
-            <form>
-                <div>
-                    <button>-</button>
-                    <input type="text" />
-                    <button>+</button>
+            <form className={styles.form}>
+                <div> 
+                    <button className={styles.decrement}>-</button>
+                    <input type="tel" />
+                    <button className={styles.increment}>+</button>
                 </div>
                 <button type="submit">Add to cart</button>
                 
