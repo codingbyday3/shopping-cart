2e6fedf4934c51f50f528a080bdf71bf54531735
Klemen <codingbyday3@users.noreply.github.com>
2025-08-05 08:46:25 +0200

    Feat: burger menu on mobile screens

--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -1,7 +1,18 @@
-import { Link,Outlet } from "react-router-dom"
+import { Link,Outlet, useLocation } from "react-router-dom"
 import styles from "./Layout.module.css"
+import { useState, useEffect } from "react"
 
 const Layout = ()=>{
+    const [showBurgerMenu, setShowBurgerMenu] = useState(false)
+    const location = useLocation()
+
+    useEffect(() =>{
+        setShowBurgerMenu(false)
+    }, [location.pathname])
+
+    const toggleBurgerMenu = ()=>{
+        setShowBurgerMenu(prev => !prev)
+    }
     return(
         <>
             <header>
@@ -10,14 +21,23 @@ const Layout = ()=>{
                 </Link>
 
                 <nav>
-                    <ul>
-                        <li>
-                            <Link className={styles.headerLink} to="/">Home Page</Link>
-                        </li>
-                        <li>
-                            <Link className={styles.headerLink}  to="/shop">Shop</Link>
-                        </li>
-                    </ul>
+                    <div className={styles.burgerMenu} onClick={toggleBurgerMenu}>
+                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+                            <title>menu</title>
+                            <path d="M3,6H21V8H3V6M3,11H21V13H3V11M3,16H21V18H3V16Z" />
+                        </svg>
+                    </div>
+                    {showBurgerMenu &&
+                        <ul id="nav">
+                            <li>
+                                <Link className={styles.headerLink} to="/">Home Page</Link>
+                            </li>
+                            <li>
+                                <Link className={styles.headerLink}  to="/shop">Shop</Link>
+                            </li>
+                        </ul>
+                    }
+
                 </nav>
             </header>
             <main>
