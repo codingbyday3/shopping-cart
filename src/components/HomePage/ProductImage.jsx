2f454e3a9f5eb9c9885ce3fd0fdcb4eaf149a649
Klemen <codingbyday3@users.noreply.github.com>
2025-08-06 18:51:07 +0200

    Feat: slider for items on homepage

--- src/components/HomePage/ProductImage.jsx
+++ src/components/HomePage/ProductImage.jsx
@@ -5,6 +5,7 @@ import styles from "./HomePage.module.css"
 const ProductImage = ()=>{
     const [items, setItems] = useState(null)
     const [loading, setLoading] = useState(true)
+    const [displayedItem, setDisplayedItem] = useState(0)
 
     useEffect(()=>{
         const getPruductImages = async ()=>{
@@ -22,6 +23,16 @@ const ProductImage = ()=>{
 
     },[])
 
+    useEffect(() =>{
+        const key = setInterval(()=>{
+            setDisplayedItem(item => (item + 1) % items.length)
+        },3000)
+
+        return ()=>{
+            clearInterval(key)
+        }
+
+    },[items])
 
     return(
         <div className={styles.imageContainer}>
@@ -29,7 +40,7 @@ const ProductImage = ()=>{
             {(loading || !items)?
                 <div>Loading...</div>
                 :
-                <img className={styles.image} src={items[1].image} alt={items[0].title} />
+                <img className={styles.image} src={items[displayedItem].image} alt={items[displayedItem].title} />
             }
         </div>
     )
